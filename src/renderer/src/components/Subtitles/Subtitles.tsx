6bb9e0e0820fad38604127c3a3ab0fab45447fd3
ahsan722505 <ahsantahir722505@gmail.com>
2024-05-25 19:03:43 +0500

    sets up UI

---
+++ src/renderer/src/components/Subtitles/Subtitles.tsx
@@ -0,0 +1,17 @@
+import UploadFile from './UploadFile/UploadFile'
+import GenerationProgress from './GenerationProgress/GenerationProgress'
+import useTranscriptionStore, { TranscriptionStatus } from '@renderer/store/transcription'
+import SubtitleList from './SubtitleList/SubtitleList'
+
+function Subtitles(): JSX.Element {
+  const status = useTranscriptionStore((state) => state.transcriptionStatus)
+  return (
+    <div className="w-full h-full">
+      {status === TranscriptionStatus.IDLE && <UploadFile />}
+      {status === TranscriptionStatus.LOADING && <GenerationProgress />}
+      {status === TranscriptionStatus.SUCCESS && <SubtitleList />}
+    </div>
+  )
+}
+
+export default Subtitles
---
+++ src/renderer/src/components/Subtitles/UploadFile/UploadFile.tsx
@@ -0,0 +1,50 @@
+import { Button, Upload, UploadProps } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
+import React from 'react'
+import useTranscriptionStore, { TranscriptionStatus } from '@renderer/store/transcription'
+
+function UploadFile(): JSX.Element {
+  const [file, setFile] = React.useState<File | null>(null)
+  const setStatus = useTranscriptionStore((state) => state.setTranscriptionStatus)
+  const setProgress = useTranscriptionStore((state) => state.setSubtitleGenerationProgress)
+  const setSubtitles = useTranscriptionStore((state) => state.setSubtitles)
+
+  const props: UploadProps = {
+    accept:
+      'audio/mp3, audio/wav, audio/mpeg, audio/ogg, audio/flac, audio/aac, audio/aiff, audio/wma, audio/opus, audio/webm, video/mp4, video/ogg, video/webm',
+    customRequest: async ({ file }) => setFile(file as File),
+    showUploadList: false
+  }
+
+  const handleCreateSubtitles = async (): Promise<void> => {
+    if (!file) return
+    setStatus(TranscriptionStatus.LOADING)
+    let progress = 0
+    const estimatedTime = 60000 // 1 minute
+    const interval = estimatedTime / 100
+    const intervalId = setInterval(() => {
+      progress += 1
+      setProgress(progress)
+      if (progress === 95) {
+        clearInterval(intervalId)
+      }
+    }, interval)
+    const transcription = await window.electron.ipcRenderer.invoke('transcribe', file.path)
+    setStatus(TranscriptionStatus.SUCCESS)
+    setSubtitles(transcription)
+  }
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-evenly">
+      <Upload {...props}>
+        <Button className="mb-4" type="primary" icon={<UploadOutlined />}>
+          Upload audio/video file
+        </Button>
+      </Upload>
+      <Button onClick={handleCreateSubtitles} disabled={!file}>
+        Create Subtitles
+      </Button>
+    </div>
+  )
+}
+
+export default UploadFile
